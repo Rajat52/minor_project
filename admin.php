612d27d610c893831a732af0c23b3e3470c37398
Rajat52 <rajatbatoe52@gmail.com>
2018-10-28 01:39:54 +0530

    new

--- admin.php
+++ admin.php
@@ -1,3 +1,36 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inventory";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "SELECT admin_id, uusername, pass FROM admin";
+if($_POST["submit"]=="login"){
+
+$usrName=$_POST["email"];
+
+$usrPwd=$_POST["password"];
+
+$sql = "SELECT * FROM admin WHERE UserName='admin@gmail.com' and Password='admin@'";
+
+$result = $con->query($sql);
+
+if ($result->num_rows > 0) {
+
+echo seccess;
+
+} else {
+
+echo 'fail';
+
+}
+
+}
+?>
 <!DOCTYPE html>
 <html>
  <head>
--- login.php
+++ login.php
@@ -1,35 +1,15 @@
-<?php  //Start the Session
-session_start();
- require('connection.php');
-//3. If the form is submitted or not.
-//3.1 If the form is submitted
-if (isset($_POST['email']) and isset($_POST['password'])){
-//3.1.1 Assigning posted values to variables.
-$username = $_POST['email'];
-$password = $_POST['password'];
-//3.1.2 Checking the values are existing in the database or not
-$query = "SELECT * FROM `user` WHERE username='$username' and password='$password'";
- 
-$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-$count = mysqli_num_rows($result);
-//3.1.2 If the posted values are equal to the database values, then session will be created for the user.
-if ($count == 1){
-$_SESSION['username'] = $username;
-}else{
-//3.1.3 If the login credentials doesn't match, he will be shown with an error message.
-$fmsg = "Invalid Login Credentials.";
-}
-}
-//3.1.4 if the user is logged in Greets the user with message
-if (isset($_SESSION['username'])){
-$username = $_SESSION['username'];
-echo "Hai " . $username . "
-";
-echo "This is the Members Area
-";
-echo "<a href='logout.php'>Logout</a>";
- 
- 
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inventory";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+?>
 <!DOCTYPE html>
 <html>
   <head>
